729dce151668a86ca08adc3d7651f0e1c2d41bf8
Teo <>
2023-11-29 17:26:28 -0300

    FEAT: Commit inicial
    esqueleto basico pelado

---
+++ OPEMAUS/src/main/java/com/teo/modelos/Persona.java
@@ -0,0 +1,17 @@
+package com.teo.modelos;
+
+import java.time.LocalDate;
+
+/**
+ * @author Teo
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+    
+    public Persona(){
+        nombre = "";
+        apellido = "";
+    }
+}
---
+++ OPEMAUS/src/main/java/com/teo/opemaus/OPEMAUS.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.teo.opemaus;
+
+import com.teo.ventanas.Logueo;
+
+/**
+ *
+ * @author ciggy
+ */
+public class OPEMAUS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Logueo.main(args);
+    }
+}
